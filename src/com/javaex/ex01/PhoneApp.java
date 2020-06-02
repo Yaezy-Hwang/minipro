80834d668e9e1bbb8660ad25ef5659f64759ad3b
'Yaezy-Hwang' <'yeahz3564@gmail.com'>
2020-06-02 19:56:29 +0900

    minipro : minipro : 1번

--- src/com/javaex/ex01/PhoneApp.java
+++ src/com/javaex/ex01/PhoneApp.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PhoneApp {
@@ -18,26 +19,13 @@ public class PhoneApp {
 
     	//1번문제
     	
-    	//키보드 입력받기 준비
-    	Scanner sc = new Scanner(System.in);
-    	
-    	//인풋스트림
-    	InputStream in = new FileInputStream("./PhoneDB.txt");
-		InputStreamReader isr = new InputStreamReader(in);
-		BufferedReader br = new BufferedReader(isr);
-		
 		//Person Arraylist 생성
 		ArrayList<Person> pList = new ArrayList<Person>();
-		//","로 잘라서 리스트에 넣기
-		while(true) {
-			String str = br.readLine();
-			if(str==null) {
-				break;
-			}
-			String[] splitInfo = str.split(",");
-			Person person = new Person(splitInfo[0], splitInfo[1], splitInfo[2]);
-			pList.add(person);
-		}
+    	
+		//파일 읽어오기
+    	readFile(pList);
+
+    	Scanner sc = new Scanner(System.in);
     	
     	System.out.println("**********************************************");
     	System.out.println("*           전화번호 관리 프로그램           *");
@@ -45,7 +33,6 @@ public class PhoneApp {
     	
     	boolean run = true;
     	while(run) {
-    		
     		System.out.println();
     		System.out.println("1.리스트   2.등록   3.삭제   4.검색   5.종료");
     		System.out.println("----------------------------------------------");
@@ -54,75 +41,19 @@ public class PhoneApp {
     		
     		switch(menu) {
     		case 1:
-    			System.out.println("<1.리스트>");
-    			for(Person p: pList) {
-    				int idnum = pList.indexOf(p)+1;
-    				System.out.println(idnum+p.showList());
-    			}
+    			showList(pList);
     			break;
     		
     		case 2:
-    			OutputStream out = new FileOutputStream("./PhoneDB.txt", true);
-    			OutputStreamWriter osw = new OutputStreamWriter(out);
-    			BufferedWriter bw = new BufferedWriter(osw); 
-
-    			System.out.println("<2.등록>");
-    			//정보입력받기
-    			System.out.print(">이름: ");
-    			String name = sc.next();
-    			
-    			
-    			System.out.print(">휴대전화: ");
-    			String hp = sc.next();
-    			
-    			System.out.print(">회사전화: ");
-    			String company = sc.next();
-
-    			//리스트에 추가
-    			Person newP = new Person(name, hp, company);
-    			pList.add(newP);
-    			//파일에 쓰기
-    			bw.write(name+","+hp+","+company);
-    			bw.newLine();
-    			
-    			System.out.println("[등록되었습니다.]");
-    			bw.close();
-
+    			addList(pList);
     			break;
     			
     		case 3:
-    			OutputStream outOver = new FileOutputStream("./PhoneDB.txt");
-    			OutputStreamWriter oswOver = new OutputStreamWriter(outOver);
-    			BufferedWriter bwOver = new BufferedWriter(oswOver); 
-    			
-       			System.out.println("<3.삭제>");
-       			System.out.print(">번호 :");
-       			int num = sc.nextInt()-1;
-       			pList.remove(num);
-       			
-       			for(Person p: pList) {
-       				bwOver.write(p.getName()+","+p.getHp()+","+p.getCompany());
-       				bwOver.newLine();
-       			}
-       			
-       			System.out.println("[삭제되었습니다.]");
-       			
-       			bwOver.close();
+    			deleteList(pList);
     			break;
     			
     		case 4:
-       			System.out.println("<4.검색>");
-       			System.out.print(">이름: ");
-       			String str = sc.next();
-    			
-       			for(Person pp: pList) {
-    				String pPart = pp.getName();
-    				if(pPart.contains(str)){
-        				int idnum = pList.indexOf(pp)+1;
-        				System.out.println(idnum+pp.showList());
-    				}
-    				
-    			}
+    			search(pList);
     			break;
     			
     		case 5:
@@ -137,15 +68,113 @@ public class PhoneApp {
     			break;
     			
     		}//switch
-    		
     	
     	}//while
-    	
-    	br.close();
+
     	sc.close();
     }
     
-
+    static Scanner sc = new Scanner(System.in);
     
+    //메소드 모음
+    
+    //인풋스트림+","로 잘라서 리스트에 넣기
+    public static void readFile(List<Person> pList) throws IOException {
+    	InputStream in = new FileInputStream("./PhoneDB.txt");
+		InputStreamReader isr = new InputStreamReader(in);
+		BufferedReader br = new BufferedReader(isr);
+
+		while(true) {
+			String str = br.readLine();
+			if(str==null) {
+				break;
+			}
+			String[] splitInfo = str.split(",");
+			Person person = new Person(splitInfo[0], splitInfo[1], splitInfo[2]);
+			pList.add(person);
+		}
+		
+    	br.close();
+    }//readFile
+    
+    
+    // 리스트 출력
+    public static void showList(List<Person> pList) throws IOException {
+    	System.out.println("<1.리스트>");
+		for(Person p: pList) {
+			System.out.println(pList.indexOf(p)+1+p.showList());
+		}
+    }//showList
+    
+    
+    // 리스트에 추가하기
+    public static void addList(List<Person> pList) throws IOException {
+    	OutputStream out = new FileOutputStream("./PhoneDB.txt", true);
+		OutputStreamWriter osw = new OutputStreamWriter(out);
+		BufferedWriter bw = new BufferedWriter(osw); 
+		
+		System.out.println("<2.등록>");
+		//정보입력받기
+		System.out.print(">이름: ");
+		String name = sc.next();
+		
+		
+		System.out.print(">휴대전화: ");
+		String hp = sc.next();
+		
+		System.out.print(">회사전화: ");
+		String company = sc.next();
+
+		//리스트에 추가
+		Person newP = new Person(name, hp, company);
+		pList.add(newP);
+		//파일에 쓰기
+		bw.write(name+","+hp+","+company);
+		bw.newLine();
+		
+		System.out.println("[등록되었습니다.]");
+		
+		bw.close();
+    }//addList
+    
+    
+    //삭제하기
+    public static void deleteList(List<Person> pList) throws IOException {
+    	OutputStream outOver = new FileOutputStream("./PhoneDB.txt");
+		OutputStreamWriter oswOver = new OutputStreamWriter(outOver);
+		BufferedWriter bwOver = new BufferedWriter(oswOver); 
+		
+		
+			System.out.println("<3.삭제>");
+			System.out.print(">번호 :");
+			int num = sc.nextInt()-1;
+			pList.remove(num);
+			
+			for(Person p: pList) {
+				bwOver.write(p.getName()+","+p.getHp()+","+p.getCompany());
+				bwOver.newLine();
+			}
+			
+			System.out.println("[삭제되었습니다.]");
+			
+			bwOver.close();
+    }//delete
+    
+    
+    //검색하기
+    public static void search(List<Person> pList) throws IOException {
+			System.out.println("<4.검색>");
+			System.out.print(">이름: ");
+			String str = sc.next();
+		
+			for(Person pp: pList) {
+			String pPart = pp.getName();
+			if(pPart.contains(str)){
+				int idnum = pList.indexOf(pp)+1;
+				System.out.println(idnum+pp.showList());
+			}
+			
+		}
+    } //search
 
 }
